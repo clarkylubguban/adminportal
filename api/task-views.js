ff5bd1f0ff278a1794d6d79c1fcf8fcfe8425b98
clarkylubguban <clarkylubguban@gmail.com>
2026-09-03 23:03:19 +0800

    feat: reuse task views function for website analytics

--- api/task-views.js
+++ api/task-views.js
@@ -1,9 +1,11 @@
 import { handleMyTasks, handleTaskCalendar } from "./_lib/taskRouteHandlers.js";
+import { handleOverviewWebsiteAnalytics } from "../server/overviewWebsiteAnalytics.js";
 
 export { handleMyTasks, handleTaskCalendar };
 
 export default function handler(request, response) {
-  const pathname = new URL(request.url || "/", "http://localhost").pathname;
-  if (pathname === "/api/task-calendar") return handleTaskCalendar(request, response);
+  const url = new URL(request.url || "/", "http://localhost");
+  if (url.searchParams.get("view") === "website-analytics") return handleOverviewWebsiteAnalytics(request, response);
+  if (url.pathname === "/api/task-calendar") return handleTaskCalendar(request, response);
   return handleMyTasks(request, response);
 }
